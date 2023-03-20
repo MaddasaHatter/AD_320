72b8e00ab028fd03206e50a4fa2e79f5a8503961
Lilith Lucifer <purpleirishlilli69@gmail.com>
2023-03-19 23:04:33 -0700

    updates

--- Assignments/fetch/mongoDBfetch.js
+++ Assignments/fetch/mongoDBfetch.js
@@ -1,31 +1,38 @@
 async function fetchData() {
     const dataFormat = {
-        "dataSource": "Cluster0",
-        "database": "sample_restaurants",
-        "collection": "restaurants",
-        "filter": {
-            "cuisine": "Italian"
-        },
+      "dataSource": "Cluster0",
+      "database": "sample_airbnb",
+      "collection": "listingsAndReviews",
+      "filter": {
+        "property_type": "Apartment"
+      },
+      "projection": {
+        "listing_url": 1,
+        "name": 1,
+        "summary": 1,
+        "property_type": 1,
+        "cancellation_policy": 1
+      }
     }
-
-    const response = await fetch('https://data.mongodb-api.com/app/data-mbose/endpoint/data/v1/action/findOne',  {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: "omit",
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Request-Headers': '*',
-            'api-key': 'VmQ4hiy8tNnWewm0TMKM5PCAdIYHg3VKMzo6EnBQvkJHUuIQ3pdEmKD2vgE1wWhu'
-        },
-
-        'redirect': 'follow',
-        'referrerPolicy': 'no-referrer',
-        'body': JSON.stringify(dataFormat)
+  
+    const response = await fetch('https://data.mongodb-api.com/app/data-mbose/endpoint/data/v1/action/findOne', {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'omit',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Request-Headers': '*',
+        'api-key': 'VmQ4hiy8tNnWewm0TMKM5PCAdIYHg3VKMzo6EnBQvkJHUuIQ3pdEmKD2vgE1wWhu'
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify(dataFormat)
     });
-
+  
     const json_response = await response.json();
     console.log(json_response);
-}
-
-fetchData();
+  }
+  
+  fetchData();
+  
